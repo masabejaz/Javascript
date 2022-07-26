a1cdf61d2b9b31973fd846e473402e148405a7fc
masabejaz <masab1059@gmail.com>
2022-07-26 10:54:28 +0500

    javascript form

--- main.js
+++ main.js
@@ -1,9 +1,27 @@
+let form = document.querySelector('#formSubmition');
+
+form.addEventListener('submit', e => {
+
+    e.preventDefault();
+
+    let fullName = document.querySelector('#fullName').value.trim();
+    let emaillAddress = document.querySelector('#emailAddress').value.trim();
 
 
+    if (fullName == '' || fullName == null) {
+        alert('Full name is empty');
+        return false;
+    }
 
-let total = 100;
+    if (emaillAddress == '' || emaillAddress == null) {
+        alert('Email is empty');
+        return false;
+    } 
 
+    let orderList = document.querySelector('#orderList');
+    console.log(orderList);
+    orderList.innerHTML += `<li>Your full name is ${fullName.toUpperCase()} and email is ${emaillAddress.toLowerCase()}</li>`;
 
-if (total >= 100) {
-    document.querySelector('.heading').innerHTML = total;
-}
+    form.reset();
+
+});
